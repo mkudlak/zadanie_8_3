c9a857025ddcfb8afcb8b644bf5d5626051e8534
Malgorzata Kudlak <m.kudlak@icloud.com>
2017-11-23 00:11:12 +0100

    Add commits to js

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,8 @@
 var name = prompt('Podaj swoje imie nieznajomy');
 alert('Witaj, ' + name);
+// linear comment Leca Gwiezdne Wojny na TVN
 console.log('Hello, ' + name);
+/*
+  block comment
+  Ble ble ble
+*/
